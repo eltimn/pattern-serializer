16dbacdc8f68e08d9d4e7e2bb9f6552ca6accb69
Tim Nelson <tnelly27@gmail.com>
2010-06-28 05:15:21 -0500

    Added Date serializer

--- src/main/scala/code/lib/PatternSerializer.scala
+++ src/main/scala/code/lib/PatternSerializer.scala
@@ -4,10 +4,11 @@ package lib {
 import net.liftweb.json.{Formats, MappingException, Serializer, TypeInfo}
 import net.liftweb.json.JsonAST._
 
+import java.util.Date
 import java.util.regex.{Pattern, PatternSyntaxException}
 
 /*
-* Provides a way to serialize/de-serialze Patterns.
+* Provides a way to serialize/de-serialize Patterns.
 *
 * Queries for a Pattern (pattern) using the lift-json DSL would look like:
 * ("pattern" -> ("$pattern" -> pattern.pattern))
@@ -56,6 +57,32 @@ class IntervalSerializer extends Serializer[Interval] {
 class Interval(start: Long, end: Long) {
   val startTime = start
   val endTime = end
+}
+
+/*
+* Provides a way to serialize/de-serialize Dates.
+*
+* Queries for a Date (dt) using the lift-json DSL would look like:
+* ("dt" -> ("$dt" -> formats.dateFormat(dt.toString)))
+*/
+class DateSerializer extends Serializer[Date] {
+  private val DateClass = classOf[Date]
+
+  def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Date] = {
+    case (TypeInfo(DateClass, _), json) => json match {
+      case JObject(List(JField("$dt", JString(s)))) =>
+        println("deserializing date: "+s)
+        format.dateFormat.parse(s) match {
+          case Some(d) => d
+          case _ => throw new MappingException("Can't parse "+ s + " to Date")
+        }
+      case x => throw new MappingException("Can't convert " + x + " to Date")
+    }
+  }
+
+  def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+    case x: Date => JObject(JField("$dt", JString(format.dateFormat.format(x))) :: Nil)
+  }
 }
 
 }
--- src/test/scala/code/SerializerExample.scala
+++ src/test/scala/code/SerializerExample.scala
@@ -1,44 +1,62 @@
 package code {
 
-import lib.{Interval, IntervalSerializer, PatternSerializer}
+import lib._
 
+import java.util.Date
 import java.util.regex.Pattern
 
 import org.specs.Specification
-  
+
+import net.liftweb.json.{NoTypeHints, Serialization}
+import net.liftweb.json.Serialization.{read, write => swrite}
+import net.liftweb.json.JsonAST._
+
 object SerializerExample extends Specification {
+
+  "Date Serializer Example" in {
+
+    implicit val formats = Serialization.formats(NoTypeHints) + new DateSerializer
+
+    /*
+    * Date serialization
+    */
+    val d = new Date
+    var dser = swrite(d)
+    dser must_== "{\"$dt\":\""+formats.dateFormat.format(d)+"\"}"
+
+    val d2 = read[Date](dser) // FIXME: throws net.liftweb.json.MappingException:
+    // Do not know how to convert JObject(List(JField($dt,JString(2010-06-28T10:09:20.255Z)))) into class java.util.Date
+    d2 must_== d
+  }
+
   "Pattern Serializer Example" in {
-  
-    import net.liftweb.json.{NoTypeHints, Serialization}
-    import net.liftweb.json.Serialization.{read, write => swrite}
-    import net.liftweb.json.JsonAST._
-    
-    implicit val formats = Serialization.formats(NoTypeHints) + new PatternSerializer + new IntervalSerializer
-      
+
+    implicit val formats = Serialization.formats(NoTypeHints) + new IntervalSerializer + new PatternSerializer
+
     /*
     * Interval serialization
-    */    
+    */
     val i = new Interval(1, 4)
     val ser = swrite(i)
     ser mustEqual """{"start":1,"end":4}"""
 
-    val i2 = read[Interval](ser) 
+    val i2 = read[Interval](ser)
     i2.startTime mustEqual i.startTime
     i2.endTime mustEqual i.endTime
-    
+
     // simple Pattern
     val p = Pattern.compile("^Curly")
-    
+
     val pser = swrite(p)
     pser must_== """{"$pattern":"^Curly"}"""
 
     val p2 = read[Pattern](pser) // FIXME: throws net.liftweb.json.MappingException: unknown error
     p2 must_== p
-    
+
     // Pattern in a case class
     case class PatternExample(id: String, regx: Pattern)
     val pe = PatternExample("1", Pattern.compile("^Moe"))
-    
+
     val peser = swrite(pe)
     peser must_== """{"id":"1","regx":{"$pattern":"^Moe"}}"""
 
@@ -48,5 +66,5 @@ object SerializerExample extends Specification {
   }
 }
 
- 
+
 }
